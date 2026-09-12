4a535cce6a1810b35e016e2af403be7c53e39748
mokshmoksh56-eng <mokshmoksh56@gmail.com>
2026-09-13 02:02:30 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,30 +1,21 @@
-// استدعاء مكتبات فايربيز لبيئة الـ Service Worker
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
 
-// نفس إعدادات فايربيز الخاصة بك
 firebase.initializeApp({
-    apiKey: "AIzaSyDmPciW_QPM4vwcMLX2M44_C0a8jqHdPjU", 
-    authDomain: "azemaa-proo.firebaseapp.com", 
-    databaseURL: "https://azemaa-proo-default-rtdb.firebaseio.com", 
-    projectId: "azemaa-proo", 
-    storageBucket: "azemaa-proo.firebasestorage.app", 
-    messagingSenderId: "787350492977", 
-    appId: "1:787350492977:web:84061dcf31622fcb07c210" 
+  apiKey: "AIzaSyDmPciW_QPM4vwcMLX2M44_C0a8jqHdPjU",
+  authDomain: "azemaa-proo.firebaseapp.com",
+  databaseURL: "https://azemaa-proo-default-rtdb.firebaseio.com",
+  projectId: "azemaa-proo",
+  storageBucket: "azemaa-proo.firebasestorage.app",
+  messagingSenderId: "787350492977",
+  appId: "1:787350492977:web:84061dcf31622fcb07c210"
 });
 
 const messaging = firebase.messaging();
-
-// استقبال الإشعارات والتطبيق مغلق
 messaging.onBackgroundMessage(function(payload) {
-    console.log('تم استقبال إشعار في الخلفية: ', payload);
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
+    self.registration.showNotification(payload.notification.title || 'طلب جديد', {
         body: payload.notification.body,
-        icon: '/icon.png', // مسار أيقونة تطبيقك (أضف صورة بصيغة png)
-        badge: '/badge.png',
+        icon: 'icon.png',
         dir: 'rtl'
-    };
-
-    self.registration.showNotification(notificationTitle, notificationOptions);
+    });
 });
