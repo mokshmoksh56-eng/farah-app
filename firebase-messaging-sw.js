b6cd2376ab603214146028743b533eafe6545d70
mokshmoksh56-eng <mokshmoksh56@gmail.com>
2026-09-13 11:36:10 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,20 +2,23 @@ importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDmPciW_QPM4vwcMLX2M44_C0a8jqHdPjU",
-  authDomain: "azemaa-proo.firebaseapp.com",
-  databaseURL: "https://azemaa-proo-default-rtdb.firebaseio.com",
-  projectId: "azemaa-proo",
-  storageBucket: "azemaa-proo.firebasestorage.app",
-  messagingSenderId: "787350492977",
-  appId: "1:787350492977:web:84061dcf31622fcb07c210"
+    apiKey: "AIzaSyDmPciW_QPM4vwcMLX2M44_C0a8jqHdPjU", 
+    authDomain: "azemaa-proo.firebaseapp.com", 
+    databaseURL: "https://azemaa-proo-default-rtdb.firebaseio.com", 
+    projectId: "azemaa-proo", 
+    storageBucket: "azemaa-proo.firebasestorage.app", 
+    messagingSenderId: "787350492977", 
+    appId: "1:787350492977:web:84061dcf31622fcb07c210"
 });
 
 const messaging = firebase.messaging();
+
 messaging.onBackgroundMessage(function(payload) {
-    self.registration.showNotification(payload.notification.title || 'طلب جديد', {
-        body: payload.notification.body,
-        icon: 'icon.png',
-        dir: 'rtl'
-    });
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png', // يمكنك تغييره برابط لوجو تطبيقك
+    dir: 'rtl'
+  };
+  return self.registration.showNotification(notificationTitle, notificationOptions);
 });
